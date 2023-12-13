a3c7c842229983a305a709a4fec00abf39dc08eb
Shajid <shajid1biz@gmail.com>
2023-12-13 14:46:22 +0530

    feat: added jakarta font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jakarta: ['Plus Jakarta Sans']
+      }
+    },
   },
   plugins: [],
 }
