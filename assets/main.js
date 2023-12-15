98a93f9ed5927ef5af8f477b2a7ddaeb00411dfe
Shajid <shajid1biz@gmail.com>
2023-12-15 10:29:51 +0530

    fixed: merged conflict issue - accepted incoming change

--- assets/main.js
+++ assets/main.js
@@ -5,552 +5,347 @@ const database = [
     accountNum: 1029384756,
     holderName: "Jack",
     holderAddress: "5396 North Reese Avenue, Fresno CA 93722",
-<<<<<<< HEAD
-    units: 121
-=======
     units: "121 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 2,
     accountNum: 1029384757,
     holderName: "Jil",
     holderAddress: "2685 California Street, Mountain View CA 94040",
-<<<<<<< HEAD
-    units: 170
-=======
     units: "170 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 3,
     accountNum: 1029384758,
     holderName: "Prince",
     holderAddress: "2443 Sierra Nevada Road, Mammoth Lakes CA 93546",
-<<<<<<< HEAD
-    units: 150
-=======
     units: "150 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 4,
     accountNum: 1029384759,
     holderName: "Jennifer Bruce",
     holderAddress: "2035 Ninth Street, Berkeley CA 94710",
-<<<<<<< HEAD
-    units: 169
-=======
     units: "169 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 5,
     accountNum: 1029384760,
     holderName: "Uriah Norton",
     holderAddress: "4439 Gale Street, Livermore CA 94550",
-<<<<<<< HEAD
-    units: 170
-=======
     units: "170 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 6,
     accountNum: 1029384761,
     holderName: "Kylee Winters",
     holderAddress: "3959 Fairlands Drive, Pleasanton CA 94588",
-<<<<<<< HEAD
-    units: 169
-=======
     units: "169 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 7,
     accountNum: 1029384762,
     holderName: "Allie Berger",
     holderAddress: "2042 High Street, Oakland CA 94601",
-<<<<<<< HEAD
-    units: 180
-=======
     units: "180 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 8,
     accountNum: 1029384763,
     holderName: "Deandre Serrano",
     holderAddress: "2335 Orchard View Lane, Escondido CA 92027",
-<<<<<<< HEAD
-    units: 190
-=======
     units: "190 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 9,
     accountNum: 1029384764,
     holderName: "Byron Osborne",
     holderAddress: "9036 Calico Court, Hesperia CA 92344",
-<<<<<<< HEAD
-    units: 200
-=======
     units: "200 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 10,
     accountNum: 1029384765,
     holderName: "Adam Randolph",
     holderAddress: "37675 Fremont Boulevard, Fremont CA 94536",
-<<<<<<< HEAD
-    units: 300
-=======
     units: "300 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
-  },
-  {
+
     id: 11,
-    accountNum: 1029384766,
     holderName: "Kailey Cochran",
     holderAddress: "10826 Pointe Royal Drive, Bakersfield CA 93311",
-<<<<<<< HEAD
-    units: 259
-=======
-    units: "259 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
+    units: "259 kWh"
   },
   {
     id: 12,
     accountNum: 1029384767,
     holderName: "Danny Conner",
     holderAddress: "6244 Sun River Drive, Sacramento CA 95824",
-<<<<<<< HEAD
-    units: 500
-=======
-    units: "500 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
+    units: "500 kWh"
   },
   {
     id: 13,
     accountNum: 1029384768,
     holderName: "Alondra Estrada",
     holderAddress: "6244 Sun River Drive, Sacramento CA 95824",
-<<<<<<< HEAD
-    units: 800
-=======
-    units: "800 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
+    units: "800 kWh"
   },
   {
     id: 14,
     accountNum: 1029384769,
     holderName: "Phoenix Donaldson",
     holderAddress: "6431 Shattuck Avenue, Oakland CA 94609",
-<<<<<<< HEAD
-    units: 799
-=======
-    units: "799 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
+    units: "799 kWh"
   },
   {
     id: 15,
     accountNum: 1029384770,
     holderName: "Natasha Dillon",
     holderAddress: "1219 Carleton Street, Berkeley CA 94702",
-<<<<<<< HEAD
-    units: 635
-=======
     units: "635 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 16,
     accountNum: 1029384771,
     holderName: "Alvin Jackson",
     holderAddress: "11522 Country Spring Court, Cupertino CA 95014",
-<<<<<<< HEAD
-    units: 115
-=======
     units: "115 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 17,
     accountNum: 1029384772,
     holderName: "Avery Banks",
     holderAddress: "1732 27th Avenue, Oakland CA 94601",
-<<<<<<< HEAD
-    units: 205
-=======
     units: "205 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 18,
     accountNum: 1029384773,
     holderName: "Martin Wu",
     holderAddress: "125 John Street, Santa Cruz CA 95060",
-<<<<<<< HEAD
-    units: 369
-=======
     units: "369 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 19,
     accountNum: 1029384774,
     holderName: "Liana Wilkinson",
     holderAddress: "1797 Pasatiempo Drive, Chico CA 95928",
-<<<<<<< HEAD
-    units: 756
-=======
     units: "756 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 20,
     accountNum: 1029384775,
     holderName: "Leonard Fowler",
     holderAddress: "1528 Stafford Avenue, Hayward CA 94541",
-<<<<<<< HEAD
-    units: 378
-=======
     units: "378 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 21,
     accountNum: 1029384776,
     holderName: "Lennon Evans",
     holderAddress: "2747 68th Avenue, Oakland CA 94605",
-<<<<<<< HEAD
-    units: 425
-=======
     units: "425 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 22,
     accountNum: 1029384777,
     holderName: "Elias Cantu",
     holderAddress: "310 12th Avenue, Santa Cruz CA 95062",
-<<<<<<< HEAD
-    units: 529
-=======
     units: "529 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 23,
     accountNum: 1029384778,
     holderName: "Galilea Walters",
     holderAddress: "6145 East Castro Valley Boulevard, Castro Valley CA 94552",
-<<<<<<< HEAD
-    units: 825
-=======
     units: "825 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 24,
     accountNum: 1029384779,
     holderName: "Colson Berg",
     holderAddress: "3835 Oakes Drive, Hayward CA 94542",
-<<<<<<< HEAD
-    units: 489
-=======
     units: "489 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 25,
     accountNum: 1029384780,
     holderName: "Emmalyn Cross",
     holderAddress: "90 Via Verde, San Lorenzo CA 94580",
-<<<<<<< HEAD
-    units: 102
-=======
     units: "102 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 26,
     accountNum: 1029384781,
     holderName: "Fabian Williamson",
     holderAddress: "918 Cheryl Ann Circle, Hayward CA 94544",
-<<<<<<< HEAD
-    units: 50
-=======
     units: "50 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 27,
     accountNum: 1029384782,
     holderName: "Catherine Rivas",
     holderAddress: "970 Old Oak Road, Livermore CA 94550",
-<<<<<<< HEAD
-    units: 801
-=======
     units: "801 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 28,
     accountNum: 1029384783,
     holderName: "Damon Waters",
     holderAddress: "2335 Orchard View Lane, Escondido CA 92027",
-<<<<<<< HEAD
-    units: 938
-=======
     units: "938 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 29,
     accountNum: 1029384784,
     holderName: "Bristol Buckley",
     holderAddress: "4439 Gale Street, Livermore CA 94550",
-<<<<<<< HEAD
-    units: 568
-=======
     units: "568 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 30,
     accountNum: 1029384785,
     holderName: "Aryan Ibarra",
     holderAddress: "4500 Margalo Avenue, Bakersfield CA 93313",
-<<<<<<< HEAD
-    units: 28
-=======
     units: "28 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 31,
     accountNum: 1029384786,
     holderName: "Madilynn Esparza",
     holderAddress: "26466 Mockingbird Lane, Hayward CA 94544",
-<<<<<<< HEAD
-    units: 478
-=======
     units: "478 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 32,
     accountNum: 1029384787,
     holderName: "Carl Strong",
     holderAddress: "968 Virginia Avenue, Olivehurst CA 95961",
-<<<<<<< HEAD
-    units: 986
-=======
     units: "986 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 33,
     accountNum: 1029384788,
     holderName: "Margo Martin",
     holderAddress: "6420 Via Baron, Rancho Palos Verdes CA 90275",
-<<<<<<< HEAD
-    units: 302
-=======
     units: "302 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 34,
     accountNum: 1029384789,
     holderName: "Mateo Stevens",
     holderAddress: "9036 Calico Court, Hesperia CA 92344",
-<<<<<<< HEAD
-    units: 289
-=======
     units: "289 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 35,
     accountNum: 1029384790,
     holderName: "Katherine Hunt",
     holderAddress: "2414 Parker Street, Berkeley CA 94704",
-<<<<<<< HEAD
-    units: 366
-=======
     units: "366 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 36,
     accountNum: 1029384791,
     holderName: "Jesus Hughes",
     holderAddress: "553 South Arlington Road, Orange CA 92869",
-<<<<<<< HEAD
-    units: 105
-=======
     units: "105 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 37,
     accountNum: 1029384792,
     holderName: "Samantha Mendez",
     holderAddress: "19416 Barclay Road, Castro Valley CA 94546",
-<<<<<<< HEAD
-    units: 204
-=======
     units: "204 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 38,
     accountNum: 1029384793,
     holderName: "Arthur Butler",
     holderAddress: "732 Kevin Court, Oakland CA 94621",
-<<<<<<< HEAD
-    units: 355
-=======
     units: "355 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 39,
     accountNum: 1029384794,
     holderName: "Athena Moran",
     holderAddress: "236 Alden Road, Hayward CA 94541",
-<<<<<<< HEAD
-    units: 789
-=======
     units: "789 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 40,
     accountNum: 1029384795,
     holderName: "Tate Norris",
     holderAddress: "5396 North Reese Avenue, Fresno CA 93722",
-<<<<<<< HEAD
-    units: 71
-=======
     units: "71 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 41,
     accountNum: 1029384796,
     holderName: "Arielle Bradley",
     holderAddress: "107 Guaymas Place, Davis CA 95616",
-<<<<<<< HEAD
-    units: 200
-=======
     units: "200 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 42,
     accountNum: 1029384797,
     holderName: "Richard Ho",
     holderAddress: "4016 Doane Street, Fremont CA 94538",
-<<<<<<< HEAD
-    units: 109
-=======
     units: "109 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 43,
     accountNum: 1029384798,
     holderName: "Calliope McKenzie",
     holderAddress: "560 Penstock Drive, Grass Valley CA 95945",
-<<<<<<< HEAD
-    units: 890
-=======
     units: "890 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 44,
     accountNum: 1029384799,
     holderName: "Scott Morgan",
     holderAddress: "1011 Devon Drive, Hayward CA 94542",
-<<<<<<< HEAD
-    units: 895
-=======
     units: "895 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 45,
     accountNum: 1029384800,
     holderName: "Delilah Strong",
     holderAddress: "36043 Soapberry Commons, Fremont CA 94536",
-<<<<<<< HEAD
-    units: 1000
-=======
     units: "1000 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 46,
     accountNum: 1029384801,
     holderName: "Axl Li",
     holderAddress: "136 Acacia Drive, Blue Lake CA 95525",
-<<<<<<< HEAD
-    units: 10
-=======
     units: "10 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 47,
     accountNum: 1029384802,
     holderName: "Paige O’Donnell",
     holderAddress: "38676 Greenwich Circle, Fremont CA 94536",
-<<<<<<< HEAD
-    units: 678
-=======
     units: "678 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 48,
     accountNum: 1029384803,
     holderName: "Lian Wall",
     holderAddress: "152 Holly Court, Mountain View CA 94043",
-<<<<<<< HEAD
-    units: 201
-=======
     units: "201 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 49,
     accountNum: 1029384804,
     holderName: "Jayda Garrett",
     holderAddress: "1314 89th Avenue, Oakland CA 94621",
-<<<<<<< HEAD
-    units: 398
-=======
     units: "398 kWh",
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
   },
   {
     id: 50,
     accountNum: 1029384804,
     holderName: "Kairo Hester",
     holderAddress: "3538 Mendenhall Court, Pleasanton CA 94588",
-<<<<<<< HEAD
-    units: 700
-  }
-];
-
-
-=======
     units: "700 kWh",
   },
 ];
@@ -577,4 +372,3 @@ function conversion(unitSplit) {
   }
 }
 conversion(unitSplit);
->>>>>>> 923786cd91de2898d01331603e9fce3eb81f4da0
